c0a84a4414644ead0cea7a33ce3f2bcffba96ac5
ankemzi <nkemzia@gmail.com>
2019-06-17 12:39:00 -0400

    Ready to deploy

--- mysite/crypto/views.py
+++ mysite/crypto/views.py
@@ -7,3 +7,15 @@ def home(request):
     api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
     api =  json.loads(api_request.content)
     return render(request, "home.html", {'api': api}) 
+
+
+def body(request):
+    import requests
+    import json
+
+    api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+    api =  json.loads(api_request.content)
+    for x in api.Data:
+        body = x.body
+        Body = body
+    return render(request, "home.html", {'Body': Body} )
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
+import django_heroku
 
 import os
 
@@ -119,3 +120,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
+django_heroku.settings(locals())
